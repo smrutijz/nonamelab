1e3875434602abd51b3a23bf97d82d0fd4130879
smrutijz <smrutijz@hotmail.com>
2021-04-27 18:54:20 +0530

    added new home template & w3.css & one immage for web page

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "I'm experimenting"
+    return render_template("home.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
